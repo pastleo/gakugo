3af856c1ff909aa05021b995711b99d93f94a25e
PastLeo <chgu82837@gmail.com>
2026-01-13 19:41:25 +0800

    add release create & seed for deployment

--- lib/gakugo/release.ex
+++ lib/gakugo/release.ex
@@ -5,6 +5,18 @@ defmodule Gakugo.Release do
   """
   @app :gakugo
 
+  def create do
+    load_app()
+
+    for repo <- repos() do
+      case repo.__adapter__.storage_up(repo.config) do
+        :ok -> :ok
+        {:error, :already_up} -> :ok
+        {:error, term} -> {:error, term}
+      end
+    end
+  end
+
   def migrate do
     load_app()
 
@@ -16,6 +28,18 @@ defmodule Gakugo.Release do
   def rollback(repo, version) do
     load_app()
     {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :down, to: version))
+  end
+
+  def seed do
+    load_app()
+
+    for repo <- repos() do
+      {:ok, _, _} =
+        Ecto.Migrator.with_repo(repo, fn _repo ->
+          seeds_path = Application.app_dir(@app, "priv/repo/seeds.exs")
+          Code.eval_file(seeds_path)
+        end)
+    end
   end
 
   defp repos do
